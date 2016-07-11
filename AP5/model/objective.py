51fea848bf26934628834968c2636db1c9a172be
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-07-11 19:39:49 -0400

    Update to new eval method

--- AP5/model/objective.py
+++ AP5/model/objective.py
@@ -62,47 +62,6 @@ def loss(hypes, logits, labels):
     return loss
 
 
-def evaluation(hypes, logits, labels):
-    """Evaluate the quality of the logits at predicting the label.
-
-    Args:
-      logits: Logits tensor, float - [batch_size, NUM_CLASSES].
-      labels: Labels tensor, int32 - [batch_size], with values in the
-        range [0, NUM_CLASSES).
-
-    Returns:
-      A scalar int32 tensor with the number of examples (out of batch_size)
-      that were predicted correctly.
-    """
-    # For a classifier model, we can use the in_top_k Op.
-    # It returns a bool tensor with shape [batch_size] that is true for
-    # the examples where the label's is was in the top k (here k=1)
-    # of all logits for that example.
-    return None
-    with tf.name_scope('eval'):
-        logits = tf.reshape(logits, (-1, 2))
-        labels = tf.reshape(labels, (-1, 2))
-
-        pred = tf.argmax(logits, dimension=1)
-
-        negativ = tf.to_int32(tf.equal(pred, 0))
-        tn = tf.reduce_sum(negativ*labels[:, 0])
-        fn = tf.reduce_sum(negativ*labels[:, 1])
-
-        positive = tf.to_int32(tf.equal(pred, 1))
-        tp = tf.reduce_sum(positive*labels[:, 1])
-        fp = tf.reduce_sum(positive*labels[:, 0])
-
-        eval_list = []
-
-        eval_list.append(('Accuracy', (tn+tp)/(tn + fn + tp + fp)))
-        eval_list.append(('Precision', tp/(tp + fp)))
-        eval_list.append(('True BG', tn/(tn + fp)))
-        eval_list.append(('True Street [Recall]', tp/(tp + fn)))
-
-        return eval_list
-
-
 def eval_image(hypes, gt_image, cnn_image):
     """."""
     thresh = np.array(range(0, 256))/255.0
@@ -117,7 +76,7 @@ def eval_image(hypes, gt_image, cnn_image):
     return FN, FP, posNum, negNum
 
 
-def tensor_eval(hypes, sess, image_pl, softmax):
+def evaluate(hypes, sess, image_pl, softmax):
     """Do evaluate in python.
 
     Parameters
